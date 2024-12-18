106bacf6d728e365b3a4f11cfc00ba682921535a
Ryan Feigum <23489211+Ryan-Feigum@users.noreply.github.com>
2024-12-18 14:14:09 -0600

    Initial commit for web site template to build site based on index.js

---
+++ webpack.dev.js
@@ -0,0 +1,24 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+});
+
+module.exports = merge(common, {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/template.html"],
+    },
+});
